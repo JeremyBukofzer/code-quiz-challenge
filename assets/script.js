939c6c72f36bc04ad1578271612b3b31a248e3c9
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-09-21 10:49:56 -0500

    adds html divs, and pseudo code in js

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// array of questions and answers
+    //question, choices, answers
+
+//start button to start quiz
+    // also starts timer
+//if statement for selecting choice goes to next question
+    // show "wrong" for incorrect choice
+    // show "correct!" for correct choice
+
+// math for timer
+    //when timer reaches 0 quiz ends
+
+//enter and save initials
+//display highscores
+
+//button to try again
